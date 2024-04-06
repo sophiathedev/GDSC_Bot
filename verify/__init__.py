8c580e425f2ea4de08df6b0b290dfb5e850a44a8
iwannabethesophia <pydevmonkey4002@gmail.com>
2024-04-06 20:33:46 +0700

    add: verify and tag role and change nickname

--- verify/__init__.py
+++ verify/__init__.py
@@ -4,11 +4,12 @@
 from typing import * # pyright: ignore
 
 import discord, asyncio
+import pandas as pd
 from discord.ext import commands, tasks
 from discord.abc import *
 
-from main import GDSCBot
-
+from main import SERVER_GUILD_ID, SERVER_VERIFY_CHANNEL_ID, SERVER_WELCOME_CHANNEL_ID
+from main import SERVER_TECH_ROLE, SERVER_CORE_ROLE, SERVER_DES_ROLE, SERVER_HR_ROLE, SERVER_PR_ROLE, SERVER_MEDIA_ROLE
 from .otp import OTP
 
 from email.mime.multipart import MIMEMultipart
@@ -44,13 +45,17 @@ class DoesntReceiveOTPView(discord.ui.View):
 # verify cog
 class Verify( commands.Cog ):
     def __init__( self, bot: commands.Bot ):
-        self.bot: GDSCBot = bot
+        # some stupid dataset
+        self.user_df = pd.read_csv('verify/data.csv')
+        self.user_df = self.user_df[["Role", "Tên", "Business Email"]]
+
+        self.bot = bot
 
         self.createGlobalVerifyMessage.start()
 
     @tasks.loop(count=1)
     async def createGlobalVerifyMessage( self ) -> None:
-        verifyChannel: discord.TextChannel = self.bot.get_guild(1220164296999436379).get_channel(1223587591677415514)
+        verifyChannel: discord.TextChannel = self.bot.get_guild(SERVER_GUILD_ID).get_channel(SERVER_VERIFY_CHANNEL_ID)
         await verifyChannel.send("**Dành cho những bạn chưa xác minh danh tính**\n\nCó một số bạn quên chưa điền mail trong quá trình xác nhận hoặc nhập mã OTP sai quá nhiều lần hoặc không nhận được mã OTP trong quá trình xác minh.\n\nCác bạn có thể tiến hành xác minh danh tính bằng cách ấn vào nút dưới đây !", view=GlobalVerifyMsgView(verifyCallback=self.verifyUser))
 
     #@commands.Cog.listener()
@@ -64,7 +69,7 @@ class Verify( commands.Cog ):
             return ("@gdscptit.dev" in m.content)
 
         # server verify channel for mention
-        verifyChannel: (GuildChannel | PrivateChannel | discord.Thread | None) = self.bot.get_channel(1220164296999436379)
+        verifyChannel: discord.TextChannel = self.bot.get_guild(SERVER_GUILD_ID).get_channel(SERVER_VERIFY_CHANNEL_ID)
 
         # get user email from user prompt
         try:
@@ -74,13 +79,21 @@ class Verify( commands.Cog ):
 
         # store as another variable for easier reading and using
         userEmail: str = userEmailMessage.content
+        searched: bool = False
+        for index in range(len(self.user_df)):
+            if userEmail == self.user_df.loc[index, "Business Email"]:
+                searched = True
+                break
+        if searched == False:
+            await member.send(f"**Không tìm thấy email trên cơ sở dữ liệu !**, Để tiến hành verify vui lòng ghé channel {verifyChannel.mention}.")
+            return None
         generatorOTP: OTP = OTP(intervalTime=900) # OTP generator with expried time is 15 minutes as 900 seconds
         # store as another variable for easie reading and using
         currentOTPCode: str = generatorOTP.currentOTP
 
         # if message sent success
         if self.sendOTP(userEmail, currentOTPCode):
-            await member.send(f"Mail chứa mã xác nhận đã được gửi tới **{userEmail}**. Vui lòng **check mail trong trang chính hoặc thư mục spam** để nhận được mã OTP có 8 chữ số.", view=DoesntReceiveOTPView(self.sendOTP, userEmail, currentOTPCode))
+            await member.send(f"Chắc hẳn bạn là **{self.user_df.loc[index, 'Tên']}** bạn cần xác minh danh tính thông qua email.\n\nMail chứa mã xác nhận đã được gửi tới **{userEmail}**. Vui lòng **check mail trong trang chính hoặc thư mục spam** để nhận được mã OTP có 8 chữ số.", view=DoesntReceiveOTPView(self.sendOTP, userEmail, currentOTPCode))
             failCount = 0
             verifyComplete = False
             countMessage: discord.Message = await member.send(f"Bạn còn **{3 - failCount}** lần thử mã.")
@@ -102,15 +115,30 @@ class Verify( commands.Cog ):
                 await countMessage.delete()
                 await member.send(f"**Xác minh danh tính thất bại**.\nĐể xác minh danh tính bạn vui lòng vào discord server của Muốn Mở Mang để xác minh lại tại channel {verifyChannel.mention}")
             else:
-                self.bot.sql.execute(f'insert or ignore into User(discord_id, email) values({member.id}, \"{userEmail}\")')
+                self.bot.sql.execute(f'insert or ignore into User(name, discord_id, email) values(\"{self.user_df.loc[index, 'Tên']}\", {member.id}, \"{userEmail}\")')
                 # welcome channel
-                welcomeChannel: discord.TextChannel = self.bot.get_guild(1220164296999436379).get_channel(1220164297506816080)
+                welcomeChannel: discord.TextChannel = self.bot.get_guild(SERVER_GUILD_ID).get_channel(SERVER_WELCOME_CHANNEL_ID)
                 # when verified complete
-                serverGuild: discord.Guild = self.bot.get_guild(1220164296999436379)
-                verifiedRole: discord.Role = serverGuild.get_role(1223587437716967534)
+                serverGuild: discord.Guild = self.bot.get_guild(SERVER_GUILD_ID)
+                #verifiedRole: discord.Role = serverGuild.get_role(1223587437716967534)
+                verifiedRole: Dict[str, discord.Role] = {
+                    "Tech": serverGuild.get_role(SERVER_TECH_ROLE),
+                    "HR": serverGuild.get_role(SERVER_HR_ROLE),
+                    "DES": serverGuild.get_role(SERVER_DES_ROLE),
+                    "PR": serverGuild.get_role(SERVER_PR_ROLE),
+                    "Media": serverGuild.get_role(SERVER_MEDIA_ROLE),
+                    "Core": serverGuild.get_role(SERVER_CORE_ROLE),
+                }
                 verifiedMember: discord.Member = member
                 try:
-                    await verifiedMember.add_roles(verifiedRole)
+                    memberRole: str = self.user_df.loc[index, 'Role']
+                    if "Lead" in memberRole or "Core" in memberRole:
+                        await verifiedMember.add_roles(verifiedRole["Core"])
+                    else:
+                        await verifiedMember.add_roles(verifiedRole[memberRole])
+                    await verifiedMember.edit(nick=f"{self.user_df.loc[index, 'Tên']} - {self.user_df.loc[index, 'Role']}")
+                    #await verifiedMember.add_roles(verifiedRole)
+                    pass
                 except discord.Forbidden as e:
                     raise e
 
