dfb2278481278c88f7a0f8badd77a49069aff8b5
iwannabethesophia <pydevmonkey4002@gmail.com>
2024-04-06 20:33:07 +0700

    edit: database table

--- gdscbot.sql
+++ gdscbot.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE User( id integer primary key autoincrement, discord_id text not null, name text default "Ẩn sĩ", email text not null, unique(id, discord_id, email));
+CREATE TABLE User( id integer primary key autoincrement, discord_id text unique not null, name text unique default "Ẩn sĩ", email text unique not null);
 CREATE TABLE Deadline(
   id integer primary key autoincrement,
   url text not null,
